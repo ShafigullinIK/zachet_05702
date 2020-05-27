aad29a6393a8f3007cea8b88a62337aa50f6fc82
Anast <anastasyakrasnova55@mail.ru>
2020-05-27 19:37:28 +0300

    done.

---
+++ src/Student.java
@@ -0,0 +1,11 @@
+public class Student
+{
+    String name;
+    int[] Otzenki;
+
+    public Student(String name, int[] Otzentki)
+    {
+        this.name = name;
+        this.Otzenki = Otzentki;
+    }
+}
